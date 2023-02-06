9b87d5fe4396109fd83efc1e2f9f29be9e0823de
[2기-1반] 강석영 <ksge1124@gmail.com>
2023-02-06 18:01:20 +0900

    create project_room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+# DONE: project_room table
+class Room(commonModel):
+    """Project_Room Model Definition"""
+
+    class ActiveTimeChoices(models.IntegerChoices):
+        ALL_TIME  = 0
+        MORNING = 9
+        AFTERNOON = 12
+        EVENING = 18
+
+    name = models.CharField(
+        max_length=180,
+        default="",
+    )
+    introduce = models.TextField()
+    recruitment_period = models.DateTimeField(
+        null=True, blank=True,
+    )
+    active_time = models.IntegerChoices(
+        choices=ActiveTimeChoices.choices,
+    )
+    main_file_key = models.CharField(
+        max_length=256,
+        null=True, blank=True,
+    )  # project_image
+    founder_id = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="rooms",
+    )
