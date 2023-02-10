c7fb5ee7919882ee9bff171a485c3099ab791488
sgaeng <ksge1124@gmail.com>
2023-02-10 18:15:38 +0900

    create User,Position,Portfoilo,UserLoginLog table

--- users/models.py
+++ users/models.py
@@ -1,90 +1,121 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from common.models import CommonModel
 
-# TODO: user table
+
+"""
+[User에서 기본적으로 제공하는 필드]
+* id (pk, not null, int)
+* username (not null, char)
+* email (char-emailField)
+* password (not null)
+* last_login (not null, dateTime)
+* date_joined (not null, dateTime)
+"""
+
 class User(AbstractUser):
-    """
-    [User에서 기본적으로 제공하는 필드]
-    * id (pk, not null, int)
-    * username (not null, char)
-    * email (char-emailField)
-    * password (not null)
-    * last_login (not null, dateTime)
-    * date_joined (not null, dateTime)
-    """
-    class LoginChoices(models.TextChoices):
-        EMAIL = ("email", "Email")
-        KAKAO = ("kakao", "Kakao")
-        NAVER = ("naver", "Naver")
-        APPLE = ("apple", "Apple")
-
-
+    """User model Definition"""
     #ref. https://velog.io/@minhyuk_ko/TIL-Enum-class-IntegerChoices
     class RoleChoices(models.IntegerChoices):
         Owner = 50
         Admin = 25
         User = 0
-
-    # user_id = models.AutoField(
-    #     primary_key=True,
-    #     unique=True,
-    # )  # id는 자동생성 필드
-
-    # nickname = models.CharField(
-    #     max_length=15,
-    # )  # 별명
-    # self_introduce = models.TextField()  # 자기소개
-    # region = models.CharField(
-    #     max_length=15,
-    # )  # 지역
-    # position = models.CharField(
-    #     max_length=17,
-    #     choices=POSITION_CHOICES,
-    # )  # 프로젝트 포지션
-    # expect_to_join = models.BooleanField(
-    #     default=False,
-    # )  # 현재 참여 가능 여부 (기본값: 현재 참여 불가능)
-    # career = models.IntegerField(
-    #     default=0,
-    # )  # 경력(연차)
-    # is_host = models.BooleanField(
-    #     default=False,
-    # )  # 방장 (기본값: 일반유저)
-    # login_method = models.CharField(
-    #     max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
-    # )  # 로그인 방법(플랫폼)
-
-    class Meta:
-        db_table = 'user'
+    
+    class RegionChoices(models.IntegerChoices):
+        Seoul = 10
+        Incheon = 20
+        Daejeon = 30
+        Daegu = 40
+        Gwangju = 50
+        Jejudo = 60
+        Ulsan = 70
+        Busan = 80
+    
+    class PlantStagesChoices(models.IntegerChoices):
+        Sprout = 0  # 새싹
+        Peak = 10  # 봉우리
+        Flower = 20  # 꽃
 
 
-# DONE: position table
+    name = models.CharField(
+        max_length=32,
+    )
+    email = models.EmailField(
+        verbose_name="email",
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(
+        max_length=32,
+        unique=True,
+    )
+    introduce = models.TextField()
+    region =  models.IntegerField(
+        choices=RegionChoices.choices,
+    )
+    available_schedule = models.BooleanField(
+        default=False,
+    )
+    career = models.PositiveIntegerField(
+        default=0,
+    )  # It allows zero.
+    profile_file_key = models.URLField(blank=True)  # For profile_img URL
+    role = models.IntegerField(
+        default=0,
+        choices=RoleChoices.choices,
+    )
+    plant_stage = models.PositiveIntegerField(
+        default=0,
+        choices=PlantStagesChoices.choices,
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )  # 방장
+    position = models.ForeignKey(
+        "users.Position",
+        on_delete=models.CASCADE,
+        related_name="user_position",
+    )
+
+
 class Position(models.Model):
     """Position Model Definition"""
     class PositionChoices(models.TextChoices):
         MANAGER = ("manager", "Manager")
         DESIGNER = ("designer", "Designer")
-        BACKEND_ENGINEER = ("backend_engineer", "Backend_Engineer")
-        FRONTEND_ENGINEER = ("frontend_engineer", "Frontend_Engineer")
-        # ENGINEER = (
-        #     BACKEND = (
-        #         "backend_engineer", "Backend_Engineer"
-        #     ),
-        #     FRONTEND = (
-        #         "frontend_engineer", "Frontend_Engineer"
-        #     )
-        # )
+        FRONTEND = ("frontend", "Frontend")
+        BACKEND = ("backend", "Backend")
     
     position = models.CharField(
-        max_length=18,
+        max_length=32,
         choices=PositionChoices.choices,
     )
-    count = models.IntegerField(
-        null=True, blank=True,
+    count = models.IntegerField()
+
+
+class Portfolio(CommonModel):
+    """Portfolio Model Definition"""
+    link = models.URLField(
+        primary_key=True,
+    )
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="portfolio",
     )
 
-# TODO: user_login_log table
+
+class UserLoginLog(models.Model):
+    """User_Login_Log Model Definition"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="userloginlog",
+    )
+    ip = models.CharField(
+        max_length=256,
+    )
 
 
 
-# TODO: portfolio table
