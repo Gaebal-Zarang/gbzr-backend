c3d59332b78dac2f691fb04924603e20bca1281a
sgaeng <ksge1124@gmail.com>
2023-02-10 18:15:08 +0900

    create Project_room, Hashtag, Connection table

--- rooms/models.py
+++ rooms/models.py
@@ -2,32 +2,51 @@ from django.db import models
 from common.models import CommonModel
 
 # DONE: project_room table
-class Room(commonModel):
+class Room(CommonModel):
     """Project_Room Model Definition"""
 
     class ActiveTimeChoices(models.IntegerChoices):
-        ALL_TIME  = 0
         MORNING = 9
         AFTERNOON = 12
         EVENING = 18
+        ALL_TIME  = 24
 
     name = models.CharField(
-        max_length=180,
-        default="",
+        max_length=256,
     )
     introduce = models.TextField()
-    recruitment_period = models.DateTimeField(
-        null=True, blank=True,
-    )
-    active_time = models.IntegerChoices(
+    recruitment_period = models.DateTimeField()
+    active_time = models.PositiveIntegerField(
+        default=0,
         choices=ActiveTimeChoices.choices,
     )
-    main_file_key = models.CharField(
-        max_length=256,
-        null=True, blank=True,
-    )  # project_image
-    founder_id = models.ForeignKey(
+    main_file_key = models.URLField(blank=True)  # For project_main_img URL
+    founder = models.ForeignKey(
         "users.User",
         on_delete=models.CASCADE,
-        related_name="rooms",
+        related_name="project_room",
+    )
+
+
+# Connection table
+class RoomPosition(models.Model):
+    """Connection Project_Room and Position Model Definition"""
+    max_applicant = models.IntegerField()
+    project_room = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        related_name="roomposition",
+    )
+    position = models.ForeignKey(
+        "users.Position",
+        on_delete=models.CASCADE,
+        related_name="roomposition",
+    )
+
+
+class Hashtag(CommonModel):
+    """Hashtag (about project) Model Definition"""
+    name = models.CharField(
+        max_length=256,
+        primary_key=True,
     )
