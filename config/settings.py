49e789b35957f787d6ddcece1c935ec4c5fc39be
sgaeng <ksge1124@gmail.com>
2023-02-15 17:19:31 +0900

    create mark model

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# DONE: db connection to postgresql
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
