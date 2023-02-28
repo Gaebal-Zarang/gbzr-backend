cd8ed6caacbc73158345580428aa9428f626bd22
sgaeng <ksge1124@gmail.com>
2023-03-01 00:09:18 +0900

    create auths app

---
+++ auths/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib.auth import logout
+
+# TODO: signup router
+class SignUpView():
+    pass
+
+
+
+# TODO: login router
+
+
+
+# TODO: logout router
+# def logout_view(request):
+#     messages.info(request, f"See you later")
+#     logout(request)
+#     return redirect(reverse("core:home"))
