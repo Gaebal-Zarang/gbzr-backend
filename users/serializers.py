46ef44014ca4949515693d3574960d7a92b3a21c
sgaeng <ksge1124@gmail.com>
2023-04-14 19:04:10 +0900

    remove signup serializers

--- users/serializers.py
+++ users/serializers.py
@@ -49,32 +49,3 @@ class PrivateUserSerializer(ModelSerializer):
             "portfolio",  # in Portfolio
             "room_founder",  # in Room
         )
-
-
-class SignupUserSerializer(ModelSerializer):
-    class Meta:
-        model = m.User
-        exclude = (
-            "id",
-            "password",
-            "last_login",
-            "is_superuser",
-            "first_name",
-            "last_name",
-            "is_staff",
-            "is_active",
-            "date_joined",
-            "groups",
-            "user_permissions",
-            "username",
-            "nickname",
-            "introduce",
-        )  # 사용하지 않을 fields
-
-        # fields = (
-        #     "name",
-        #     "email",
-        #     "password",
-        # )
-
-        # fields = "__all__"
