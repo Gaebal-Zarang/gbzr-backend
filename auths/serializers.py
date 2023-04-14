b83a51a613c7672e45e411872e66bc35e7e753dc
sgaeng <ksge1124@gmail.com>
2023-04-14 19:03:04 +0900

    create signup serializers

---
+++ auths/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework.serializers import ModelSerializer
+from users import models as m
+
+
+class SignupUserSerializer(ModelSerializer):
+    class Meta:
+        model = m.User
+        exclude = (
+            "id",
+            "password",
+            "last_login",
+            "is_superuser",
+            "first_name",
+            "last_name",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "groups",
+            "user_permissions",
+            "username",
+            "nickname",
+            "introduce",
+        )  # 사용하지 않을 fields
+
+        # fields = (
+        #     "name",
+        #     "email",
+        #     "password",
+        # )
+
+        # fields = "__all__"
